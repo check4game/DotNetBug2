8694214449039b1e15892152dbdf1dd80e9522d8
check4game <maxim.amsd@gmail.com>
2025-01-20 12:46:37 +0300

    version 1

--- DotNetBug2.cs
+++ DotNetBug2.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Runtime.Intrinsics;
 
-BenchmarkRunner.Run(typeof(Program).Assembly);
+var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
 
 [SimpleJob(RunStrategy.Monitoring, launchCount: 1, iterationCount: 10, warmupCount: 5, invocationCount: 1)]
 [Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
@@ -41,55 +41,66 @@ public class HashTableBenchmark
     }
 
     [Benchmark(Baseline = true)]
-    public int AddOptimal()
+    public int AddVectorCPUIndexOf()
     {
         foreach (var key in data.AsSpan().Slice(0, (int)(ht.Capacity * Load)))
         {
-            ht.AddOptimal(key);
+            ht.AddVectorCPUIndexOf(key);
         }
 
         return ht.Count;
     }
 
     [Benchmark]
-    public int AddOptimalFix1()
+    public int AddVector()
     {
         foreach (var key in data.AsSpan().Slice(0, (int)(ht.Capacity * Load)))
         {
-            ht.AddOptimalFix1(key);
+            ht.AddVector(key);
         }
 
         return ht.Count;
     }
 
     [Benchmark]
-    public int AddOptimalFix2()
+    public int AddVectorIF()
     {
         foreach (var key in data.AsSpan().Slice(0, (int)(ht.Capacity * Load)))
         {
-            ht.AddOptimalFix1(key);
+            ht.AddVectorIF(key);
         }
 
         return ht.Count;
     }
 
     [Benchmark]
-    public int AddOptimalFix3()
+    public int AddVectorWhile()
     {
         foreach (var key in data.AsSpan().Slice(0, (int)(ht.Capacity * Load)))
         {
-            ht.AddOptimalFix3(key);
+            ht.AddVectorWhile(key);
         }
 
         return ht.Count;
     }
 
     [Benchmark]
-    public int AddOptimalCPU()
+    public int AddVectorCPUFor()
     {
         foreach (var key in data.AsSpan().Slice(0, (int)(ht.Capacity * Load)))
         {
-            ht.AddOptimalCPU(key);
+            ht.AddVectorCPUFor(key);
+        }
+
+        return ht.Count;
+    }
+
+    [Benchmark]
+    public int AddCPU()
+    {
+        foreach (var key in data.AsSpan().Slice(0, (int)(ht.Capacity * Load)))
+        {
+            ht.AddCPU(key);
         }
 
         return ht.Count;
@@ -104,11 +115,9 @@ class HashTable
 
         _lengthMinusOne = (_Capacity - 1);
 
-        _controlBytes = GC.AllocateUninitializedArray<byte>((int)_Capacity + _vectorSize);
+        _controlBytes = GC.AllocateArray<byte>((int)_Capacity + _vectorSize);
 
         _entries = GC.AllocateUninitializedArray<uint>((int)_Capacity + _vectorSize);
-
-        Clear();
     }
 
     public uint Capacity => _Capacity;
@@ -122,7 +131,7 @@ class HashTable
     private Vector128<byte> _emptyBucketVector = Vector128.Create(_emptyBucket);
     public void Clear()
     {
-        Count = 0; _controlBytes.AsSpan().Fill(_emptyBucket);
+        Count = 0; _controlBytes.AsSpan().Clear();
     }
 
     private byte[] _controlBytes;
@@ -132,24 +141,24 @@ class HashTable
     public int Count { get; private set; } = 0;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    internal static uint ResetLowestSetBit(uint value) => value & (value - 1);
+    private static uint ResetLowestSetBit(uint value) => value & (value - 1);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static ref T Find<T>(T[] array, ulong index)
+    private static ref T GetReference<T>(T[] array, ulong index)
     {
         ref var arr0 = ref MemoryMarshal.GetArrayDataReference(array);
         return ref Unsafe.Add(ref arr0, (nuint)index);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static ref byte Find(byte[] array, ulong index)
+    private static ref byte GetReference(byte[] array, ulong index)
     {
         ref var arr0 = ref MemoryMarshal.GetArrayDataReference(array);
         return ref Unsafe.AddByteOffset(ref arr0, (nuint)index);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static byte FindValue(byte[] array, ulong index)
+    private static byte GetValue(byte[] array, ulong index)
     {
         ref var arr0 = ref MemoryMarshal.GetArrayDataReference(array);
         return Unsafe.AddByteOffset(ref arr0, (nuint)index);
@@ -160,10 +169,11 @@ class HashTable
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static ulong GetHashCode(uint key) => ((uint)key.GetHashCode() * _goldenRatio);
 
-    private static byte H2(ulong hashcode) => (byte)((hashcode >> 55) | 0x01);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static byte H2(ulong hashcode) => (byte)((hashcode >> 56) | 0x01);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void AddOptimal(uint key)
+    public void AddVector(uint key)
     {
         var index = GetHashCode(key);
 
@@ -179,7 +189,7 @@ class HashTable
         {
             index &= _lengthMinusOne; // Use bitwise AND to ensure the index wraps around within the bounds of the map. Thus preventing out of bounds exceptions
 
-            var source = Vector128.LoadUnsafe(ref Find(_controlBytes, index));
+            var source = Vector128.LoadUnsafe(ref GetReference(_controlBytes, index));
 
             // Compare `source` and `target` vectors to find any positions with a matching control byte.
             var resultMask = Vector128.Equals(source, target).ExtractMostSignificantBits();
@@ -191,7 +201,7 @@ class HashTable
                 var bitPos = BitOperations.TrailingZeroCount(resultMask);
 
                 // If a matching key is found, update the entry's value and return the old value.
-                if (Find(_entries, index + (uint)bitPos) == key)
+                if (GetReference(_entries, index + (uint)bitPos) == key)
                 {
                     return;
                 }
@@ -207,8 +217,8 @@ class HashTable
             {
                 index += (uint)BitOperations.TrailingZeroCount(emptyMask);
 
-                Find(_controlBytes, index) = h2;
-                Find(_entries, index) = key;
+                GetReference(_controlBytes, index) = h2;
+                GetReference(_entries, index) = key;
 
                 Count++;
 
@@ -221,7 +231,7 @@ class HashTable
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void AddOptimalFix1(uint key)
+    public void AddVectorIF(uint key)
     {
         var index = GetHashCode(key);
 
@@ -237,7 +247,7 @@ class HashTable
         {
             index &= _lengthMinusOne; // Use bitwise AND to ensure the index wraps around within the bounds of the map. Thus preventing out of bounds exceptions
 
-            var source = Vector128.LoadUnsafe(ref Find(_controlBytes, index));
+            var source = Vector128.LoadUnsafe(ref GetReference(_controlBytes, index));
 
             // Compare `source` and `target` vectors to find any positions with a matching control byte.
             var resultMask = Vector128.Equals(source, target).ExtractMostSignificantBits();
@@ -249,7 +259,7 @@ class HashTable
                 var bitPos = BitOperations.TrailingZeroCount(resultMask);
 
                 // If a matching key is found, update the entry's value and return the old value.
-                if (Find(_entries, index + (uint)bitPos) == key)
+                if (GetReference(_entries, index + (uint)bitPos) == key)
                 {
                     return;
                 }
@@ -263,13 +273,13 @@ class HashTable
             // Check for empty buckets in the current vector.
             if (emptyMask != 0)
             {
-                if (_emptyBucket != FindValue(_controlBytes, index))
+                if (_emptyBucket != GetValue(_controlBytes, index))
                 {
                     index += (uint)BitOperations.TrailingZeroCount(emptyMask);
                 }
 
-                Find(_controlBytes, index) = h2;
-                Find(_entries, index) = key;
+                GetReference(_controlBytes, index) = h2;
+                GetReference(_entries, index) = key;
 
                 Count++;
 
@@ -282,7 +292,7 @@ class HashTable
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void AddOptimalFix2(uint key)
+    public void AddVectorWhile(uint key)
     {
         var index = GetHashCode(key);
 
@@ -298,7 +308,7 @@ class HashTable
         {
             index &= _lengthMinusOne; // Use bitwise AND to ensure the index wraps around within the bounds of the map. Thus preventing out of bounds exceptions
 
-            var source = Vector128.LoadUnsafe(ref Find(_controlBytes, index));
+            var source = Vector128.LoadUnsafe(ref GetReference(_controlBytes, index));
 
             // Compare `source` and `target` vectors to find any positions with a matching control byte.
             var resultMask = Vector128.Equals(source, target).ExtractMostSignificantBits();
@@ -310,7 +320,7 @@ class HashTable
                 var bitPos = BitOperations.TrailingZeroCount(resultMask);
 
                 // If a matching key is found, update the entry's value and return the old value.
-                if (Find(_entries, index + (uint)bitPos) == key)
+                if (GetReference(_entries, index + (uint)bitPos) == key)
                 {
                     return;
                 }
@@ -324,10 +334,10 @@ class HashTable
             // Check for empty buckets in the current vector.
             if (emptyMask != 0)
             {
-                while (_emptyBucket != FindValue(_controlBytes, index)) index++;
+                while (_emptyBucket != GetValue(_controlBytes, index)) index++;
 
-                Find(_controlBytes, index) = h2;
-                Find(_entries, index) = key;
+                GetReference(_controlBytes, index) = h2;
+                GetReference(_entries, index) = key;
 
                 Count++;
 
@@ -340,7 +350,7 @@ class HashTable
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void AddOptimalFix3(uint key)
+    public void AddVectorCPUFor(uint key)
     {
         var index = GetHashCode(key);
 
@@ -371,7 +381,7 @@ class HashTable
                 var bitPos = BitOperations.TrailingZeroCount(resultMask);
 
                 // If a matching key is found, update the entry's value and return the old value.
-                if (Find(_entries, index + (uint)bitPos) == key)
+                if (GetReference(_entries, index + (uint)bitPos) == key)
                 {
                     return;
                 }
@@ -380,11 +390,11 @@ class HashTable
                 resultMask = ResetLowestSetBit(resultMask);
             }
 
-            for (byte i = 0; i < _vectorSize; i++)
+            for (byte pos = 0; pos < _vectorSize; pos++)
             {
                 if (_emptyBucket == cb)
                 {
-                    cb = h2; Find(_entries, index + i) = key; Count++; return;
+                    cb = h2; GetReference(_entries, index + pos) = key; Count++; return;
                 }
 
                 cb = Unsafe.AddByteOffset(ref cb, 1);
@@ -396,11 +406,14 @@ class HashTable
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void AddOptimalCPU(uint key)
+    public void AddVectorCPUIndexOf(uint key)
     {
         var index = GetHashCode(key);
 
         var h2 = H2(index);
+
+        // Create a SIMD vector with the value of 'h2' for quick equality checks.
+        var target = Vector128.Create(h2);
 
         // Initialize the probing jump distance to zero, which will increase with each probe iteration.
         byte jumpDistance = 0;
@@ -411,25 +424,101 @@ class HashTable
         {
             index &= _lengthMinusOne; // Use bitwise AND to ensure the index wraps around within the bounds of the map. Thus preventing out of bounds exceptions
 
+            var source = Vector128.LoadUnsafe(ref Unsafe.AddByteOffset(ref controlBytes, (nuint)index));
+
+            // Compare `source` and `target` vectors to find any positions with a matching control byte.
+            var resultMask = Vector128.Equals(source, target).ExtractMostSignificantBits();
+
+            // Loop over each set bit in `mask` (indicating matching positions).
+            while (resultMask != 0)
+            {
+                // Find the lowest set bit in `mask` (first matching position).
+                var bitPos = BitOperations.TrailingZeroCount(resultMask);
+
+                // If a matching key is found, update the entry's value and return the old value.
+                if (GetReference(_entries, index + (uint)bitPos) == key)
+                {
+                    return;
+                }
+
+                //// Clear the lowest set bit in `mask` to continue with the next matching bit.
+                resultMask = ResetLowestSetBit(resultMask);
+            }
+
+            var pos = IndexOf(ref Unsafe.AddByteOffset(ref controlBytes, (nuint)index));
+
+            if (pos != byte.MaxValue)
+            {
+                Unsafe.AddByteOffset(ref controlBytes, (nuint)index + pos) = h2;
+                GetReference(_entries, index + pos) = key; Count++; return;
+            }
+
+            jumpDistance += _vectorSize; // Increase the jump distance by 16 to probe the next cluster.
+            index += jumpDistance; // Move the index forward by the jump distance.           
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static byte IndexOf(ref byte cb)
+    {
+        if (cb == _emptyBucket) return 0;
+
+        var ul = Unsafe.As<byte, ulong>(ref cb);
+
+        //if (((ul >> 56) & 0xFF) == _emptyBucket) return 0;
+        if (((ul >> 48) & 0xFF) == _emptyBucket) return 1;
+        if (((ul >> 40) & 0xFF) == _emptyBucket) return 2;
+        if (((ul >> 32) & 0xFF) == _emptyBucket) return 3;
+        if (((ul >> 24) & 0xFF) == _emptyBucket) return 4;
+        if (((ul >> 16) & 0xFF) == _emptyBucket) return 5;
+        if (((ul >>  8) & 0xFF) == _emptyBucket) return 6;
+        if (((ul      ) & 0xFF) == _emptyBucket) return 7;
+
+        ul = Unsafe.As<byte, ulong>(ref Unsafe.AddByteOffset(ref cb, 8));
+
+        if (((ul >> 56) & 0xFF) == _emptyBucket) return 8;
+        if (((ul >> 48) & 0xFF) == _emptyBucket) return 9;
+        if (((ul >> 40) & 0xFF) == _emptyBucket) return 10;
+        if (((ul >> 32) & 0xFF) == _emptyBucket) return 11;
+        if (((ul >> 24) & 0xFF) == _emptyBucket) return 12;
+        if (((ul >> 16) & 0xFF) == _emptyBucket) return 13;
+        if (((ul >>  8) & 0xFF) == _emptyBucket) return 14;
+        if (((ul      ) & 0xFF) == _emptyBucket) return 15;
+
+        return byte.MaxValue;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void AddCPU(uint key)
+    {
+        var index = GetHashCode(key);
+
+        var h2 = H2(index);
+
+        // Initialize the probing jump distance to zero, which will increase with each probe iteration.
+        byte jumpDistance = 0, pos;
+
+        ref var controlBytes = ref MemoryMarshal.GetArrayDataReference(_controlBytes);
+
+        while (true)
+        {
+            index &= _lengthMinusOne; // Use bitwise AND to ensure the index wraps around within the bounds of the map. Thus preventing out of bounds exceptions
+
             ref var cb = ref Unsafe.AddByteOffset(ref controlBytes, (nuint)index);
 
-            for (byte pos = 0; pos < _vectorSize; pos++)
+            for (pos = 0; pos < _vectorSize; pos++)
             {
-                if (h2 == cb && Find(_entries, index + pos) == key) return;
+                if (h2 == cb && GetReference(_entries, index + pos) == key) return;
 
                 cb = Unsafe.AddByteOffset(ref cb, 1);
             }
 
-            cb = Unsafe.AddByteOffset(ref cb, -_vectorSize);
+            pos = IndexOf(ref Unsafe.AddByteOffset(ref controlBytes, (nuint)index));
 
-            for (byte pos = 0; pos < _vectorSize; pos++)
+            if (pos != byte.MaxValue)
             {
-                if (_emptyBucket == cb)
-                {
-                    cb = h2; Find(_entries, index + pos) = key; Count++; return;
-                }
-
-                cb = Unsafe.AddByteOffset(ref cb, 1);
+                Unsafe.AddByteOffset(ref controlBytes, (nuint)index + pos) = h2;
+                GetReference(_entries, index + pos) = key; Count++; return;
             }
 
             jumpDistance += _vectorSize; // Increase the jump distance by 16 to probe the next cluster.
